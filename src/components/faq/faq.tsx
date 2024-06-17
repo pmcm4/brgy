3d21ab0a6c599ad8b96be7fcbb73a15c3c15d539
pmcm4 <pmorato187@gmail.com>
2024-06-17 15:20:33 +0800

    change accordion to mui component

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -1,6 +1,10 @@
 import classNames from 'classnames';
 import styles from './faq.module.scss';
 import { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export interface FAQProps {
     className?: string;
@@ -31,51 +35,147 @@ export const FAQ = ({ questions, heading, subheading, className }: FAQProps) => 
             </div>
             <div className={styles['faq-body']}>
                 <div className={styles['left']}>
-                    {questions.slice(0, 5).map((obj, index) => (
-                        <div key={obj.faqid} className={styles['accordion-div']}>
-                            <button
-                                className={styles.accordion}
-                                value={obj.faqid}
-                                onClick={() => handleActive(obj.faqid)}
-                            >
-                                {obj.faqTitle}
-                            </button>
-                            <div
-                                className={
-                                    activeIndices.includes(obj.faqid)
-                                        ? styles['active']
-                                        : styles['panel']
-                                }
-                            >
-                                <span className={styles['answer']}>{obj.answer}</span>
-                            </div>
-                        </div>
-                    ))}
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            What is Lorem Ipsum?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text ever
+                            since the 1500s,
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Where does it come from?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It
+                            has roots in a piece of classical Latin literature from 45 BC, making it
+                            over 2000 years old. Richard McClintock, a Latin professor at
+                            Hampden-Sydney College in Virginia, looked up one of the more obscure
+                            Latin words, consectetur, from a Lorem Ipsum passage, and going through
+                            the cites of the word in classical literature, discovered the
+                            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+                            of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+                            Cicero,{' '}
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Why do we use it?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            It is a long established fact that a reader will be distracted by the
+                            readable content of a page when looking at its layout. The point of
+                            using Lorem Ipsum is that it has a more-or-less normal distribution of
+                            letters, as opposed to using 'Content here, content here', making it
+                            look like readable English. Many desktop publishing packages and web
+                            page editors now use Lorem Ipsum as their default model text, and a
+                            search for 'lorem ipsum' will uncover many web sites still in their
+                            infancy. Various versions have evolved over the years, sometimes by
+                            accident, sometimes on purpose (injected humour and the like).
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Where can I get some?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            There are many variations of passages of Lorem Ipsum available, but the
+                            majority have suffered alteration in some form, by injected humour, or
+                            randomised words which don't look even slightly believable.
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
                 <div className={styles['right']}>
-                    {questions.slice(5, 10).map((obj, index) => (
-                        <div key={obj.faqid} className={styles['accordion-div']}>
-                            <button
-                                className={styles.accordion}
-                                value={obj.faqid}
-                                onClick={() => handleActive(obj.faqid)}
-                            >
-                                {obj.faqTitle}
-                            </button>
-                            <div
-                                className={
-                                    activeIndices.includes(obj.faqid)
-                                        ? styles['active']
-                                        : styles['panel']
-                                }
-                            >
-                                <span className={styles['answer']}>{obj.answer}</span>
-                            </div>
-                        </div>
-                    ))}
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            What is Lorem Ipsum?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting
+                            industry. Lorem Ipsum has been the industry's standard dummy text ever
+                            since the 1500s,
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Where does it come from?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Contrary to popular belief, Lorem Ipsum is not simply random text. It
+                            has roots in a piece of classical Latin literature from 45 BC, making it
+                            over 2000 years old. Richard McClintock, a Latin professor at
+                            Hampden-Sydney College in Virginia, looked up one of the more obscure
+                            Latin words, consectetur, from a Lorem Ipsum passage, and going through
+                            the cites of the word in classical literature, discovered the
+                            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+                            of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+                            Cicero,{' '}
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Why do we use it?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            It is a long established fact that a reader will be distracted by the
+                            readable content of a page when looking at its layout. The point of
+                            using Lorem Ipsum is that it has a more-or-less normal distribution of
+                            letters, as opposed to using 'Content here, content here', making it
+                            look like readable English. Many desktop publishing packages and web
+                            page editors now use Lorem Ipsum as their default model text, and a
+                            search for 'lorem ipsum' will uncover many web sites still in their
+                            infancy. Various versions have evolved over the years, sometimes by
+                            accident, sometimes on purpose (injected humour and the like).
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion className={styles['accordion']}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Where can I get some?
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            There are many variations of passages of Lorem Ipsum available, but the
+                            majority have suffered alteration in some form, by injected humour, or
+                            randomised words which don't look even slightly believable.
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
             </div>
-            ;
+
             <div className={styles['faq-breaker']}>
                 <img
                     src="https://www.geocities.ws/jkandres/portfolio/marikina/190.seal.gif"
@@ -96,3 +196,50 @@ export const FAQ = ({ questions, heading, subheading, className }: FAQProps) => 
         </div>
     );
 };
+
+{
+    /* <div className={styles['left']}>
+{questions.slice(0, 5).map((obj, index) => (
+    <div key={obj.faqid} className={styles['accordion-div']}>
+        <button
+            className={styles.accordion}
+            value={obj.faqid}
+            onClick={() => handleActive(obj.faqid)}
+        >
+            {obj.faqTitle}
+        </button>
+        <div
+            className={
+                activeIndices.includes(obj.faqid)
+                    ? styles['active']
+                    : styles['panel']
+            }
+        >
+            <span className={styles['answer']}>{obj.answer}</span>
+        </div>
+    </div>
+))}
+</div>
+<div className={styles['right']}>
+{questions.slice(5, 10).map((obj, index) => (
+    <div key={obj.faqid} className={styles['accordion-div']}>
+        <button
+            className={styles.accordion}
+            value={obj.faqid}
+            onClick={() => handleActive(obj.faqid)}
+        >
+            {obj.faqTitle}
+        </button>
+        <div
+            className={
+                activeIndices.includes(obj.faqid)
+                    ? styles['active']
+                    : styles['panel']
+            }
+        >
+            <span className={styles['answer']}>{obj.answer}</span>
+        </div>
+    </div>
+))}
+</div> */
+}
